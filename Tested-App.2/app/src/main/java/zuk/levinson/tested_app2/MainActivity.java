8a635722cf6fa3ab5c8eb0a11360c2debde4a147
ZukLevinson <zuklevinson@gmail.com>
2017-09-22 20:05:51 +0300

    App-Files Upload

---
+++ Tested-App.2/app/src/main/java/zuk/levinson/tested_app2/MainActivity.java
@@ -0,0 +1,13 @@
+package zuk.levinson.tested_app2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
